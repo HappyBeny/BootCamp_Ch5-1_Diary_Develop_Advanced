57da6fe7852eef93aaf18affb7309d32f57dc23d
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2025-01-06 13:32:36 +0900

    ⚡ perf : 레벨 4:N+1 문제 개선하기

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -4,10 +4,12 @@ import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
+    @Query("SELECT t FROM Todo t JOIN FETCH t.user ORDER BY t.modifiedAt DESC")
+    Page<Todo> findAllWithUserByOrderByModifiedAtDesc(Pageable pageable);
 
     int countById(Long todoId);
 }
